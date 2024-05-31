d6ff1f1bb904a631eda09c2efd655ce1d4bfbd6a
brunobrun <45899738+brunobrun@users.noreply.github.com>
2024-05-31 14:41:06 +0200

    Update Repl_tutorial.py

--- Repl_tutorial.py
+++ Repl_tutorial.py
@@ -1,1 +1,4 @@
+##The Python REPL(Read Evaluate Print Loop)###
+##It’s an interactive shell that allows you to enter Python commands and directly see the results.###
+##It’s a great way to tinker and learn! We’ll use the REPL as a calculator and explore Python’s operators.###
 
