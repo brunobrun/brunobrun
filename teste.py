a6651f1041558ea5d8eb3eff6633f7e6129569f6
brunobrun <45899738+brunobrun@users.noreply.github.com>
2024-05-31 14:15:48 +0200

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,8 @@
 ##################################
-#Primeiro programa em Python
+#Primeiro programa em Python#####
 
 print('Hello world')
+print('My name is Bruno')
+print('Vou me tornar mestre em usar GitHub')
+#################################
+#
